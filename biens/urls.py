8cf6751982a953165bb51f01afe5bd974053962a
angelbelando <angel.belando@orange.fr>
2025-01-29 19:15:44 +0100

    genereic

--- biens/urls.py
+++ biens/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'biens'
 urlpatterns = [
     
-    path('objets', views.ObjetList.as_view(), name='liste_objets'),
+    path('objets/', views.ObjetListView.as_view(), name='liste_objets'),
+    
+    path('objets/<int:pk>/', views.ObjetDetailView.as_view(), name='detail_objet'),
+    
     # path('<pk>', views.DetailFilm.as_view(), name='detail'),
 ]
--- biens/views.py
+++ biens/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Objet
 from .forms import ObjetForm  
-from django.views import generic  
+from django.views.generic import ListView, DetailView
 
 def create_objet(request):
     if request.method == 'POST':
@@ -14,13 +14,12 @@ def create_objet(request):
         form = ObjetForm()
     return render(request, 'create_objet.html', {'form': form})
 
-class ObjetList(generic.ListView):
+class ObjetListView(ListView):
     model = Objet
     context_object_name = "objets"
     template_name = 'liste_objets.html'
-    queryset = Objet.objects.all().select_related()
-    paginate_by = 20
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+
+
+class ObjetDetailView(DetailView):
+    model = Objet
+    template_name = 'detail_objet.html'
