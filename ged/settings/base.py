02e787db5547c2a09551ebff948ebfd9a7a53fda
angelbelando <angel.belando@orange.fr>
2025-02-17 17:28:21 +0100

    Test_serveur

--- ged/settings/base.py
+++ ged/settings/base.py
@@ -15,7 +15,14 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Définir BASE_DIR au répertoire racine du projet Django
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Autres configurations...
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
