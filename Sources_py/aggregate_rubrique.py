2e49b4da006c5d379b8b1eb9460cfedf7bdc514d
angelbelando <angel.belando@orange.fr>
2025-02-17 16:00:11 +0100

    init_base

--- Sources_py/aggregate_rubrique.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import Sum
-from biens.models import Objet
-
-resultats = Objet.objects.values('rubrique').annotate(total_montant=Sum('montant'))
-for resultat in resultats:
-    print(f"Rubrique: {resultat['rubrique']}, Total: {resultat['total_montant']}")
