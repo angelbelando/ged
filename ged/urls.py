c20ffc88a45fbabd621cd7640aff3f6df1517412
angelbelando <angel.belando@orange.fr>
2026-05-26 16:20:25 +0200

    JWT

--- ged/urls.py
+++ ged/urls.py
@@ -19,7 +19,9 @@ from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
 from django.views.generic import TemplateView
-from biens import views as biens_views 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from biens import views as biens_views
 from django.conf.urls.static import static
 from django.views import generic
 from django.contrib.auth import views as auth_views
@@ -40,7 +42,8 @@ urlpatterns = [
     path('protected/', biens_views.ProtectedView.as_view(), name='protected'),
     path('photologue/', include('photologue.urls', namespace='photologue')),
     #path('api-token-auth/', obtain_auth_token),
-    
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
